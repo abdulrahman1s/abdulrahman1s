b209c97e0346a91bee65dfd7dd30172b0493168b
Abdulrahman Salah <61483023+abdulrahman1s@users.noreply.github.com>
2022-10-06 17:42:19 +0200

    Update update-feedback.py

--- scripts/update-feedback.py
+++ scripts/update-feedback.py
@@ -5,12 +5,14 @@ username = "abdulrahman1s"
 regex = r"<!--{{feedback_start}}-->[\s\S\n]+<!--{{feedback_end}}-->"
 reviews = requests.get(f"https://abdulrahman1s-fiverr-api.deno.dev/{username}/reviews").json()
 
+
 def format_review(review):
     username = review["username"]
     comment = review["comment"]
-    return f"- {username}: **{comment}**"
+    return f"- [@{username}](https://fiverr.com/{username}): **{comment}**"
 
-content = '\n'.join(map(format_review, reviews))
+
+content = '\n'.join(map(format_review, filter(lambda r: r["value"] >= 4, reviews)))
 
 
 with open("README.md", "r+") as file:
