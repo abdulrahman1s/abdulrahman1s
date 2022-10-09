3486ea2e939ae7021444b23b3b441ea9adbe5aab
Abdulrahman Salah <61483023+abdulrahman1s@users.noreply.github.com>
2022-10-10 00:55:16 +0200

    Update github-feedback.py

--- scripts/github-feedback.py
+++ scripts/github-feedback.py
@@ -7,7 +7,7 @@ review =  {
             "user_id": env["USER_ID"],
             "username": env["USERNAME"],
             "avatar": env["AVATAR_URL"] if "AVATAR_URL" in env else None,
-            "content": '\n'.join(env["BODY"].splitlines()[3:]),
+            "content": '\n'.join(env["BODY"].splitlines()[2:]),
             "created_at": env["CREATED_AT"],
             "source": "github",
 }
