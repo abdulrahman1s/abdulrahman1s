98212642f4c29e6becac7b44fd7c8bdc673d5f0d
Abdulrahman Salah <61483023+abdulrahman1s@users.noreply.github.com>
2022-10-10 00:18:30 +0200

    Update fiverr-feedback.py

--- scripts/fiverr-feedback.py
+++ scripts/fiverr-feedback.py
@@ -1,24 +1,33 @@
 import requests
-import re
+import json
 
 username = "abdulrahman1s"
-limit = 100
-reviews = requests.get(f"https://abdulrahman1s-fiverr-api.deno.dev/{username}/reviews?limit={limit}").json()
+limit = 20
+req = requests.get(f"https://abdulrahman1s-fiverr-api.deno.dev/{username}/reviews?limit={limit}").json()
 
+def transform(reviews):
+    reviews = filter(lambda r: r["value"] >= 4, reviews)
 
-def format_review(review):
-    username = review["username"]
-    comment = review["comment"]
-    return f"- [@{username}](https://fiverr.com/{username}): **{comment}**"
+    def into_review(r):
+        return {
+            "username": r["username"],
+            "avatar": r["user_image_url"] if "user_image_url" in r else None,
+            "content": r["comment"],
+            "created_at": r["created_at"],
+            "source": "fiverr",
+        }
 
+    reviews = map(into_review, reviews)
+    return reviews
 
-content = '\n'.join(map(format_review, filter(lambda r: r["value"] >= 4, reviews)))
+with open("assets/feedback.json", "r+") as file:
+   feedback = {}
+   array = json.loads(file.read())
+   array.extend(transform(req))
 
-with open("README.md", "r+") as file:
-   file_content = file.read()
-   file_content = re.sub(
-               r"<!--fiverr_feedback_start-->[\s\S\n]+<!--fiverr_feedback_end-->", 
-               f"<!--fiverr_feedback_start-->\n{content}\n<!--fiverr_feedback_end-->", file_content)
+   # Remove duplicates
+   for r in array:
+      feedback[r["username"]] = r
 
    file.seek(0)
-   file.write(file_content)
+   file.write(json.dumps(list(feedback.values())))
