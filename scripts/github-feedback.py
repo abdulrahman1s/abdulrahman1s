2df8b74f50f6d22491db892538df7fc98ddb5bb5
Abdulrahman Salah <61483023+abdulrahman1s@users.noreply.github.com>
2022-10-10 00:30:54 +0200

    Update github-feedback.py

--- scripts/github-feedback.py
+++ scripts/github-feedback.py
@@ -1,5 +1,25 @@
-import re
+import json
 import os
 
+env = os.environ
 
-print(os.environ["ISSUE_BODY"])
+review =  {
+            "user_id": env["USER_ID"],
+            "username": env["USERNAME"],
+            "avatar": env["AVATAR_URL"] if "AVATAR_URL" in env else None,
+            "content": env["BODY"],
+            "created_at": env["CREATED_AT"],
+            "source": "github",
+}
+
+with open("assets/feedback.json", "r+") as file:
+   feedback = {}
+   array = json.loads(file.read())
+   array.append(review)
+
+   # Remove duplicates
+   for r in array:
+      feedback[r["username"]] = r
+
+   file.seek(0)
+   file.write(json.dumps(list(feedback.values())))
