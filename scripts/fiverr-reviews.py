77f3e79d174233a1412af031a9c4f9936a07eeb3
Abdulrahman Salah <61483023+abdulrahman1s@users.noreply.github.com>
2022-10-09 23:06:25 +0200

    Update and rename update-feedback.py to fiverr-reviews.py

--- scripts/fiverr-reviews.py
+++ scripts/fiverr-reviews.py
@@ -2,8 +2,8 @@ import requests
 import re
 
 username = "abdulrahman1s"
-regex = r"<!--{{feedback_start}}-->[\s\S\n]+<!--{{feedback_end}}-->"
-reviews = requests.get(f"https://abdulrahman1s-fiverr-api.deno.dev/{username}/reviews").json()
+limit = 100
+reviews = requests.get(f"https://abdulrahman1s-fiverr-api.deno.dev/{username}/reviews?limit={limit}").json()
 
 
 def format_review(review):
@@ -14,9 +14,11 @@ def format_review(review):
 
 content = '\n'.join(map(format_review, filter(lambda r: r["value"] >= 4, reviews)))
 
-
 with open("README.md", "r+") as file:
    file_content = file.read()
-   file_content = re.sub(regex, "<!--{{feedback_start}}-->\n" + content + "\n<!--{{feedback_end}}-->", file_content)
+   file_content = re.sub(
+               r"<!--{{fiverr_start}}-->[\s\S\n]+<!--{{fiverr_end}}-->", 
+               "<!--{{fiverr_start}}-->\n" + content + "\n<!--{{fiverr_end}}-->", file_content)
+
    file.seek(0)
    file.write(file_content)
