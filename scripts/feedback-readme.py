0e4cad84620fa99ff2877bab08c545c6ea7c92fd
Abdulrahman Salah <61483023+abdulrahman1s@users.noreply.github.com>
2022-10-10 01:11:02 +0200

    Update feedback-readme.py

--- scripts/feedback-readme.py
+++ scripts/feedback-readme.py
@@ -3,6 +3,21 @@ import json
 
 feedback = json.loads(open("assets/feedback.json", "r").read())
 
+def format(review):
+    username = review["username"]
+    content = review["content"]
+    source = review["source"]
+    url = ""
+    
+    if source == "github":
+       url = f"https://github.com/{username}"
+    elif source == "fiverr":
+       url = f"https://fiverr.com/{username}"
+
+    return f"- [@{username}]({url}): **{content}**"
+
+feedback = '\n'.join(map(format, feedback))
+
 with open("README.md", "r+") as file:
      content = file.read()
      content = re.sub(r"<!--feedback_start-->[\s\S\n]+<!--feedback_end-->", f"<!--feedback_start-->{content}<!--feedback_end-->", content)
